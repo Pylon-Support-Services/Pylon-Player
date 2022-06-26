be52de8447180172b09c618a2a7b34d726815641
Jeremiah Cruickshank <Jeremiahv8@hotmail.com>
2022-06-26 07:01:00 -0400

    master

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script id="js" type="text/javascript" src="index.js"></script>
+<link rel="stylesheet" type="text/css" href="index.css">
+
+<title>Account</title>
+</head>
+<body>
+    <div id="Platform">
+        <div id="Players" class="Container"></div>
+        <div id="Playlist" class="Container"></div>
+        <div id="Content" class="Container"></div>
+    </div>
+</body>
+</html>
